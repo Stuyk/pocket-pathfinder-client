5b7e391c2102ba247da40f0ef04683f7ab55f62d
Trevor Wessel <stuykgaming@gmail.com>
2020-11-01 12:34:35 -0700

    PATHS

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? 'pocket-pathfinder-client' : ''
+    publicPath: process.env.NODE_ENV === 'production' ? '/pocket-pathfinder-client/' : '/'
 };

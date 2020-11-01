1105554cdd16c4b85176c9da85ee2254ec3d9356
Trevor Wessel <stuykgaming@gmail.com>
2020-11-01 10:29:27 -0700

    init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$event = new Vue();
 
 new Vue({
-  router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    router,
+    render: function(h) {
+        return h(App);
+    }
+}).$mount('#app');
---
+++ src/views/Landing.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.svg" width="400" />
+        <Connection />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Connection from '@/components/Connection.vue';
+
+export default {
+    name: 'Landing',
+    components: {
+        Connection
+    }
+};
+</script>
